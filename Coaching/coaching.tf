5d941dcfcba60b9cc9e1631629151ceb5d989fae
João Amaral <62885027+joaooamaral@users.noreply.github.com>
2020-12-14 17:33:49 +0000

    Update coaching.tf
    - Supports TF 0.14
    - Added a Public IP to the SQL Server
    - Updated version of azurerm API and random provider
    - Added a possibility to provision the resources in UAE region

--- Coaching/coaching.tf
+++ Coaching/coaching.tf
@@ -20,41 +20,42 @@ variable "client-name" {
 }
 variable "location" {
   type        = string
-  description = "Enter the Azure Region for the creation of your Coaching Resource Group. Ex: West Europe / East US / East Asia"
+  description = "Enter the Azure Region for the creation of your Coaching Resource Group. Ex: West Europe / East US / East Asia / UAE North"
   # Variable validation is currently in Beta - TF accepts only re2 based Regex
   validation {
-    condition     = can(regex("(?i)(\\W|^)(westeurope|eastus|eastasia|west\\seurope|east\\sus|east\\sasia)(\\W|$)", var.location))
-    error_message = "The location must be either West Europe, East US or East Asia."
+    condition     = can(regex("(?i)(\\W|^)(westeurope|eastus|eastasia|uaenorth|west\\seurope|east\\sus|east\\sasia|uae\\snorth)(\\W|$)", var.location))
+    error_message = "The location must be either West Europe, East US, East Asia or UAE North ."
   }
 }
 variable "coaching-persons" {
   type        = number
   description = "Enter how many people in total will be in the coaching"
 }
-variable "vm-admin" {
-  type        = string
-  description = "Enter the Administrator username for the VMs"
-}
-variable "vm-password" {
-  type        = string
-  description = "Enter the Administrator password for the VMs"
-}
 #############################################################################
 # PROVIDER
 #############################################################################
-provider "azurerm" {
-  version = "=2.5.0"
-  features {}
-}
-provider "random" {
-  version = "~> 2.2"
-}
 terraform {
-  experiments = [variable_validation]
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.40.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
 }
 #############################################################################
 # RESOURCES
 #############################################################################
+locals {
+    admin_username = "oscoaching"
+    admin_password = "P@$$w0rd1234!"
+}
 resource "azurerm_resource_group" "rg-coaching" {
   name     = "${var.client-name}-Coaching"
   location = var.location
@@ -65,7 +66,7 @@ resource "random_integer" "vnet-address-space" {
 }
 resource "azurerm_virtual_network" "vnet-coaching" {
   name                = "VNet-${var.client-name}-Coaching"
-  address_space       = ["10.${random_integer.vnet-address-space.result}.0.0/16"] #["10.180.0.0/16"]   #To be decided based on a random number
+  address_space       = ["10.${random_integer.vnet-address-space.result}.0.0/16"]  #To be decided based on a random number
   location            = var.location
   resource_group_name = azurerm_resource_group.rg-coaching.name
 }
@@ -73,7 +74,7 @@ resource "azurerm_subnet" "snet-coaching" {
   name                 = "SNet-${var.client-name}-Coaching"
   resource_group_name  = azurerm_resource_group.rg-coaching.name
   virtual_network_name = azurerm_virtual_network.vnet-coaching.name
-  address_prefix       = cidrsubnet("10.${random_integer.vnet-address-space.result}.0.0/16", 8, 2) #"10.180.1.0/24"    #To be decided based on a random number
+  address_prefixes     = ["10.${random_integer.vnet-address-space.result}.1.0/24"]  #To be decided based on a random number
   service_endpoints    = ["Microsoft.Sql", "Microsoft.Web"]
 }
 resource "azurerm_network_security_group" "nsg-coaching" {
@@ -105,7 +106,7 @@ resource "azurerm_network_security_rule" "nsg-coaching-fe-in-https" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "80"
+  destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg-coaching.name
@@ -124,9 +125,29 @@ resource "azurerm_network_security_rule" "nsg-coaching-fe-in-rdp" {
   resource_group_name         = azurerm_resource_group.rg-coaching.name
   network_security_group_name = azurerm_network_security_group.nsg-coaching.name
 }
+resource "azurerm_network_security_rule" "nsg-coaching-fe-in-mssql" {
+  name                        = "MSSQL"
+  priority                    = 998
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "1433"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg-coaching.name
+  network_security_group_name = azurerm_network_security_group.nsg-coaching.name
+}
 resource "azurerm_public_ip" "pip-coaching" {
   count                   = var.coaching-persons * 3
   name                    = "Pip-Coaching-${count.index}"
+  location                = var.location
+  resource_group_name     = azurerm_resource_group.rg-coaching.name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+}
+resource "azurerm_public_ip" "pip-sql" {
+  name                    = "Pip-Coaching-SQL"
   location                = var.location
   resource_group_name     = azurerm_resource_group.rg-coaching.name
   allocation_method       = "Dynamic"
@@ -142,7 +163,7 @@ resource "azurerm_network_interface" "nic-coaching" {
     name                          = "Nic-${count.index}-Coaching-ipconfig"
     subnet_id                     = azurerm_subnet.snet-coaching.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip-coaching["${count.index}"].id
+    public_ip_address_id          = azurerm_public_ip.pip-coaching[count.index].id
   }
 }
 resource "azurerm_network_interface" "nic-sql" {
@@ -154,6 +175,7 @@ resource "azurerm_network_interface" "nic-sql" {
     name                          = "Nic-SQL-Coaching-ipconfig"
     subnet_id                     = azurerm_subnet.snet-coaching.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-sql.id
   }
 }
 resource "azurerm_virtual_machine" "vm-coaching" {
@@ -161,7 +183,7 @@ resource "azurerm_virtual_machine" "vm-coaching" {
   name                             = "VM-${count.index}-Coaching"
   location                         = var.location
   resource_group_name              = azurerm_resource_group.rg-coaching.name
-  network_interface_ids            = [azurerm_network_interface.nic-coaching["${count.index}"].id]
+  network_interface_ids            = [azurerm_network_interface.nic-coaching[count.index].id]
   vm_size                          = "Standard_B2s" #2vCPUs / 4 GB RAM
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
@@ -176,8 +198,8 @@ resource "azurerm_virtual_machine" "vm-coaching" {
   }
   os_profile {
     computer_name  = "VM-${count.index}-Coaching"
-    admin_username = var.vm-admin
-    admin_password = var.vm-password
+    admin_username = local.admin_username
+    admin_password = local.admin_password
   }
   os_profile_windows_config {
     enable_automatic_upgrades = false
@@ -185,48 +207,40 @@ resource "azurerm_virtual_machine" "vm-coaching" {
   }
 }
 
-resource "azurerm_virtual_machine" "vm-sql-coaching" {
+resource "azurerm_windows_virtual_machine" "vm-sql-coaching" {
   name                  = "SQL-Coaching"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg-coaching.name
   network_interface_ids = [azurerm_network_interface.nic-sql.id]
-  vm_size               = "Standard_B2ms"
-  storage_image_reference {
+  size                  = "Standard_B2ms"
+  admin_username        = local.admin_username
+  admin_password        = local.admin_password
+  source_image_reference {
     publisher = "MicrosoftSQLServer"
     offer     = "SQL2016SP2-WS2016"
     sku       = "Standard"
-    version   = "13.2.200310"
+    version   = "latest"
   }
-  storage_os_disk {
-    name              = "VM-SQL-Coaching-OsDisk"
-    caching           = "ReadOnly"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "VM-SQL-Coaching"
-    admin_username = var.vm-admin
-    admin_password = var.vm-password
-  }
-  os_profile_windows_config {
-    provision_vm_agent        = true
-    enable_automatic_upgrades = false
+  os_disk {
+    name                 = "VM-SQL-Coaching-OsDisk"
+    caching              = "ReadOnly"
+    storage_account_type = "Standard_LRS"
   }
 }
 resource "azurerm_mssql_virtual_machine" "sql-instance-coaching" {
-  virtual_machine_id               = azurerm_virtual_machine.vm-sql-coaching.id
+  virtual_machine_id               = azurerm_windows_virtual_machine.vm-sql-coaching.id
   sql_license_type                 = "PAYG"
   sql_connectivity_port            = 1433
   sql_connectivity_type            = "PRIVATE"
-  sql_connectivity_update_password = var.vm-password
-  sql_connectivity_update_username = var.vm-admin
+  sql_connectivity_update_password = local.admin_password
+  sql_connectivity_update_username = local.admin_username
 }
 #############################################################################
 # OUTPUTS
 #############################################################################
 output "RG-Name" {
   description = "Coaching Resource Group name"
-  value       = azurerm_resource_group.rg-coaching.name
+  value       = "Coaching Resource Group name: ${azurerm_resource_group.rg-coaching.name}"
 }
 output "FEs-IPs" {
   description = "IPs of all FEs provisoned."
@@ -234,5 +248,9 @@ output "FEs-IPs" {
 }
 output "SQL-IP" {
   description = "Coaching's SQL Server IP."
-  value       = azurerm_network_interface.nic-sql.private_ip_addresses
+  value       = "Private IP: ${azurerm_network_interface.nic-sql.private_ip_address} / Public IP: ${azurerm_windows_virtual_machine.vm-sql-coaching.public_ip_address}"
+}
+output "vm-creds" {
+  description = "VMs Credentials"
+  value       = "The credentials to access all the servers (SQL included) are: ${local.admin_username} / ${local.admin_password}"
 }
