31940fc7121bf224750d43486fdb539cc1ed8d1c
João Amaral <62885027+joaooamaral@users.noreply.github.com>
2021-04-09 18:36:54 +0100

    Update ops_coaching_setup.ps1

--- powershell/ops_coaching_setup.ps1
+++ powershell/ops_coaching_setup.ps1
@@ -1,2 +1,16 @@
 #Format the data disk and create partition
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -DriveLetter "O" -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "OutSystems" -Confirm:$false
+
+#Disable IE Enhanced Security
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+}
+function Disable-UserAccessControl {
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000 -Force
+}
+Disable-UserAccessControl
+Disable-InternetExplorerESC
