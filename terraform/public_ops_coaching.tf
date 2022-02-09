b6e52db4092cc79c4d1dddfea6c1fe2215f8d930
joaooamaral <joao.amaral@outsystems.com>
2022-02-09 11:04:31 +0000

    Upgraded azurerm version

--- terraform/public_ops_coaching.tf
+++ terraform/public_ops_coaching.tf
@@ -28,7 +28,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.91.0"
+      version = "=2.95.0"
     }
     random = {
       source  = "hashicorp/random"
